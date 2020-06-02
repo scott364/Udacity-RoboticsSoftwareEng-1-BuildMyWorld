e8f50f7f61e6f3a8af3753fc20dac72f0a0394a7
Scott Scheraga <scottscheraga@gmail.com>
2020-06-01 20:36:18 -0600

    Add files via upload

---
+++ script/welcome.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginTutorial : public WorldPlugin
+  {
+    public: WorldPluginTutorial() : WorldPlugin()
+            {
+              printf("Welcome to Scott Scheraga's World!");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
+}
